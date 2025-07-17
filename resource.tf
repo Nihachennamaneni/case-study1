b582ebef9d5d6a5ea6cb68ae3a162c064d8379c8
ravinderrao6789@gmail.com <ravinderrao6789@gmail.com>
2025-07-17 15:40:24 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -9,3 +9,8 @@ resource "azurerm_storage_account" "saterra" {
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
+resource "azurerm_storage_container" "example" {
+  name                  = "cnblob"
+  storage_account_name  = azurerm_storage_account.example.name
+  container_access_type = "private"
+}
