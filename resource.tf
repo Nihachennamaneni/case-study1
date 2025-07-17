aaf0e60013283eac4251a08745d535a4faab549f
ravinderrao6789@gmail.com <ravinderrao6789@gmail.com>
2025-07-17 11:51:17 +0000

    latestcode

--- resource.tf
+++ resource.tf
@@ -2,10 +2,10 @@ resource "azurerm_resource_group" "terra-casestudy" {
   name     = "terra-casestudy"
   location = "East US"
 } 
-resource "azurerm_storage_account" "chnkds2582" {
+resource "azurerm_storage_account" "saterra" {
     name                     = "chnkds2582"
-    resource_group_name      = azurerm_resource_group.storeterra.name
-    location                 = azurerm_resource_group.storeterra.location
+    resource_group_name      = azurerm_resource_group.terra-casestudy.name
+    location                 = azurerm_resource_group.terra-casestudy.location
     account_tier             = "Standard"
     account_replication_type = "LRS"
 }
