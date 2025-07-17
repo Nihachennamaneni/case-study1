3a4ff1df4d8b137e50cd4fadedcf1c3835dd692c
ravinderrao6789@gmail.com <ravinderrao6789@gmail.com>
2025-07-17 15:42:19 +0000

    latest

--- resource.tf
+++ resource.tf
@@ -11,6 +11,6 @@ resource "azurerm_storage_account" "saterra" {
 }
 resource "azurerm_storage_container" "example" {
   name                  = "cnblob"
-  storage_account_name  = azurerm_storage_account.example.name
+  storage_account_name  = azurerm_storage_account.saterra.name
   container_access_type = "private"
 }
